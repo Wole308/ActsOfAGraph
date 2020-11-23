c1de0fb1503d67ace820ef20b384e3ea934e3054
Wole <oj2zf@virginia.edu>
2020-11-23 13:44:31 -0500

    ...

---
+++ ReadMes/submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=test
+#SBATCH --output=res.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --time=10:00
+#SBATCH --mem-per-cpu=100
+
+srun hostname
+pwd
+srun sleep 6
+nvcc --version
